7ea8d0decc08c643e412f7d64465ccc71ad7f05f
Anand Alexander Samaroo <74620779+aasamaroo@users.noreply.github.com>
2021-07-09 17:38:59 -0400

    Add files via upload

---
+++ lab7/coe628lab7/src/coe628/lab7/Counter.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coe628.lab7;
+
+/**
+ *
+ * @author aasamaro
+ */
+/** Maintains a count. */
+public class Counter {
+    int count = 0;
+    /**
+     * Add value to count.
+     * @param value int to add to count
+     */
+    public synchronized void add(int value) {
+        this.count += value;
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException ex) {
+            System.err.println("Should not get here!" + ex);
+        }
+    }
+}
